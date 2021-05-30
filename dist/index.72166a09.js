180e99d5cf0af389615f3d57fb138999c2e2d351
parssa-airborneapp <parssak@gmail.com>
2021-05-30 09:49:41 -0400

    added comments

--- dist/index.72166a09.js
+++ dist/index.72166a09.js
@@ -690,40 +690,57 @@ const OrbitControls = _threeOrbitControlsDefault.default(_three);
 class CameraController {
   constructor(scene) {
     this.scene = scene;
-    // -- Camera Settings --
-    this.fov = 45;
-    this.near = 0.01;
-    this.far = 20000;
     // Renderer
+    this.AddRenderer();
+    // Set up Camera
+    this.AddCamera();
+    // Set up Controls
+    this.AddControls();
+    // Recalibrates on resize
+    window.addEventListener('resize', () => this.HandleResize());
+  }
+  /** Initializes Renderer and appends
+  *  it to the #container element.
+  */
+  AddRenderer() {
     this.renderer = new _three.WebGLRenderer({
       antialias: true
     });
     this.renderer.setSize(window.innerWidth, window.innerHeight);
-    // Container
     this.container = document.getElementById('container');
     this.container.appendChild(this.renderer.domElement);
-    // Set up Camera
+  }
+  /** Initializes Camera*/
+  AddCamera() {
+    this.fov = 45;
+    this.near = 0.01;
+    this.far = 20000;
     this.camera = new _three.PerspectiveCamera(this.fov, window.innerWidth / window.innerHeight, this.near, this.far);
-    // Set up Controls
+  }
+  /** Initializes Controls*/
+  AddControls() {
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-    // Optional Controls Settings
     this.controls.autoRotate = false;
     this.controls.enableDamping = true;
     this.controls.dampingFactor = 0.39;
     this.camera.position.z = 200;
     this.controls.update();
-    // Recalibrates on resize
-    window.addEventListener('resize', () => this._Resize());
   }
-  Update() {
-    this.controls.update();
-    this.renderer.render(this.scene, this.camera);
-  }
-  _Resize() {
+  /** Handles recalibrating the camera when
+  *  the window is resized.
+  */
+  HandleResize() {
     this.camera.aspect = this.container.clientWidth / this.container.clientHeight;
     this.camera.updateProjectionMatrix();
     this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
     this.renderer.setPixelRatio(window.devicePixelRatio);
+  }
+  /** Updates the controls, and re-renders everything.
+  *  Note: This is called by the Scene update loop externally.
+  */
+  Update() {
+    this.controls.update();
+    this.renderer.render(this.scene, this.camera);
   }
 }
 class Scene {
@@ -734,12 +751,16 @@ class Scene {
     this.scene = new _three.Scene();
     // Camera Controller
     this.cameraController = new CameraController(this.scene);
+    this.Setup();
+    // Run the Update loop
+    this.cameraController.renderer.setAnimationLoop(time => this.Update(time));
+  }
+  /** Include any Scene setup logic here*/
+  Setup() {
     const ambientLight = new _three.AmbientLight(0x041f60);
     ambientLight.intensity = 0.3;
     this.scene.add(ambientLight);
     this.scene.fog = new _three.Fog(0x041f60, 3000, 20000);
-    // Run the Update loop
-    this.cameraController.renderer.setAnimationLoop(time => this.Update(time));
   }
   /**
   * Adds a new mesh to the scene.
--- src/setup.js
+++ src/setup.js
@@ -16,48 +16,67 @@ class CameraController {
   constructor(scene) {
     this.scene = scene;
 
-    // -- Camera Settings -- 
+    // Renderer
+    this.AddRenderer()
+
+    // Set up Camera
+    this.AddCamera();
+
+    // Set up Controls
+    this.AddControls();
+
+    // Recalibrates on resize
+    window.addEventListener('resize', () => this.HandleResize())
+  }
+
+  /** Initializes Renderer and appends 
+   *  it to the #container element.
+  */
+  AddRenderer() {
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.container = document.getElementById('container')
+    this.container.appendChild(this.renderer.domElement)
+  }
+
+  /** Initializes Camera */
+  AddCamera() {
     this.fov = 45;
     this.near = 0.01;
     this.far = 20000;
 
-    // Renderer
-    this.renderer = new THREE.WebGLRenderer({ antialias: true });
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    
-    // Container
-    this.container = document.getElementById('container')
-    this.container.appendChild(this.renderer.domElement)
-
-    // Set up Camera
     this.camera = new THREE.PerspectiveCamera(this.fov, window.innerWidth / window.innerHeight, this.near, this.far);
+  }
 
-    // Set up Controls
+  /** Initializes Controls */
+  AddControls() {
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 
-    // Optional Controls Settings
     this.controls.autoRotate = false;
     this.controls.enableDamping = true;
     this.controls.dampingFactor = 0.39;
     this.camera.position.z = 200;
 
     this.controls.update();
-
-    // Recalibrates on resize
-    window.addEventListener('resize', () => this._Resize())
   }
 
-  Update() {
-    this.controls.update();
-    this.renderer.render(this.scene, this.camera);
-  }
-
-  _Resize() {
+  /** Handles recalibrating the camera when
+   *  the window is resized.
+   */
+  HandleResize() {
     this.camera.aspect = this.container.clientWidth / this.container.clientHeight;
     this.camera.updateProjectionMatrix();
 
     this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
     this.renderer.setPixelRatio(window.devicePixelRatio);
+  }
+
+  /** Updates the controls, and re-renders everything.
+   *  Note: This is called by the Scene update loop externally.
+   */
+  Update() {
+    this.controls.update();
+    this.renderer.render(this.scene, this.camera);
   }
 }
 class Scene {
@@ -71,15 +90,19 @@ class Scene {
     // Camera Controller
     this.cameraController = new CameraController(this.scene);
 
-    const ambientLight = new THREE.AmbientLight(0x041f60);
-    ambientLight.intensity = 0.3;
-    this.scene.add(ambientLight);
-    this.scene.fog = new THREE.Fog(0x041f60, 3000, 20000);
+    this.Setup()
 
     // Run the Update loop
     this.cameraController.renderer.setAnimationLoop(time => this.Update(time))
   }
 
+  /** Include any Scene setup logic here */
+  Setup() {
+    const ambientLight = new THREE.AmbientLight(0x041f60);
+    ambientLight.intensity = 0.3;
+    this.scene.add(ambientLight);
+    this.scene.fog = new THREE.Fog(0x041f60, 3000, 20000);
+  }
 
   /**
    * Adds a new mesh to the scene.
